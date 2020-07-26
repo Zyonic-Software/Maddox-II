ef9055120150928948c5c70c93dbd9d17301309b
AzraAnimating <azraanimating@gmail.com>
2020-07-26 11:00:21 +0200

    Version

--- src/main/java/com/zyonicsoftware/maddox/modules/listener/GuildMemberLeaveListener.java
+++ src/main/java/com/zyonicsoftware/maddox/modules/listener/GuildMemberLeaveListener.java
@@ -1,6 +1,7 @@
 /*
  * Zyonic Software - 2020 - Tobias Rempe
- * This File, its contents and by extention the corresponding project may be used freely in compliance with the Apache 2.0 License.
+ * This File, its contents and by extention the corresponding project may be
+ * used freely in compliance with the Apache 2.0 License.
  *
  * tobiasrempe@zyonicsoftware.com
  */
